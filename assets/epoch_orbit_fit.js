616457f6199f3e66d88e77c21c2e655c15f94d11
Nick Swainston <nickaswainst@gmail.com>
2023-11-09 12:41:34 +0800

    Made the multiple sliders work

--- assets/epoch_orbit_fit.js
+++ assets/epoch_orbit_fit.js
@@ -13,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const initialSin = [xRange.map(x => sinEquation(x, initialAmplitude, initialPeriod, initialPhase))];
   console.log(initialSin);
 
+  let amplitude = initialAmplitude;
+  let period = initialPeriod;
+  let phase = initialPhase;
+
 
   const trace = {
     x: xRange,
@@ -20,24 +24,57 @@ document.addEventListener('DOMContentLoaded', function () {
     type: 'scatter',
   };
 
-  const sliderSteps = [];
-  for (let a = 0; a <= 5; a += 0.1) {
-    sliderSteps.push({
+
+  const amplitudeSliderSteps = [];
+  const periodSliderSteps = [];
+  const phaseSliderSteps = [];
+  for (let a = 0; a <= 10; a += 0.1) {
+    amplitudeSliderSteps.push({
       method: 'relayout',
       label: a.toFixed(1),
-      args: ['x', [xRange.map(x => sinEquation(x, a, initialPeriod, initialPhase))]],
+      args: ['yaxis.range', [-10, 10]],
+    });
+    periodSliderSteps.push({
+      method: 'relayout',
+      label: 2 * a.toFixed(1),
+      args: ['yaxis.range', [-10, 10]],
+    });
+    phaseSliderSteps.push({
+      method: 'relayout',
+      label: a.toFixed(1),
+      args: ['yaxis.range', [-10, 10]],
     });
   }
 
   const sliders = [
     {
-      steps: sliderSteps,
+      steps: amplitudeSliderSteps,
       active: 10,  // Initial position of the slider
       currentvalue: {
-        prefix: 'a: ',
-        xanchor: 'right',
+        prefix: 'Amplitude: ',
+        xanchor: 'left',
         visible: true,
         offset: 25,
+      },
+    },
+    {
+      steps: periodSliderSteps,
+      active: 10,  // Initial position of the slider
+      currentvalue: {
+        prefix: 'Period: ',
+        xanchor: 'center',
+        visible: true,
+        offset: 75,
+      },
+    },
+    {
+      steps: phaseSliderSteps,
+      active: 10,  // Initial position of the slider
+      currentvalue: {
+        prefix: 'Phase: ',
+        xanchor: 'right',
+        visible: true,
+        offset: 125,
       },
     },
   ];
@@ -53,8 +90,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const plot = document.getElementById('plot');
 
   plot.on('plotly_sliderchange', (event) => {
-    const a = event.step.label;
-    const newY = xRange.map(x => sinEquation(x, parseFloat(a), initialPeriod, initialPhase));
+    console.log(event);
+    if (event.slider.currentvalue.prefix.startsWith('Amplitude')) {
+      amplitude = parseFloat(event.step.label);
+    }
+    if (event.slider.currentvalue.prefix.startsWith('Period')) {
+      period = parseFloat(event.step.label);
+    }
+    if (event.slider.currentvalue.prefix.startsWith('Phase')) {
+      phase = parseFloat(event.step.label);
+    }
+    const newY = xRange.map(x => sinEquation(x, amplitude, period, phase));
     Plotly.update(plot, { y: [newY] });
   });
 });
