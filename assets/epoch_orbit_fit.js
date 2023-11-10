db6d21b0674a677633afbc3fd2ada415431f777e
NickSwainston <nickaswainston@gmail.com>
2023-11-10 10:25:16 +0800

    Used some real data but need to fix phase

--- assets/epoch_orbit_fit.js
+++ assets/epoch_orbit_fit.js
@@ -22,135 +22,186 @@ function residual_calc(data, model) {
   return result;
 }
 
+// Function to read the file
+function readFile(filePath) {
+  return fetch(filePath)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch the file: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(content => {
+      const { days, residuals } = parseDataFile(content);
+
+      // Display the result
+      return { days, residuals };
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+// Function to parse the data file content into two arrays of floats
+function parseDataFile(content) {
+  const lines = content.trim().split('\n');
+  const days = [];
+  const residuals = [];
+
+  lines.forEach(line => {
+    const [value1, value2] = line.split(' ').map(parseFloat);
+    days.push(value1);
+    residuals.push(value2*10e3);
+  });
+
+  return { days, residuals };
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-  const xRange = range(1, 100);
-  const initialAmplitude = 1;
-  const initialPeriod = 30;
-  const initialPhase = 10;
-  const initialSin = [xRange.map(x => sinEquation(x, initialAmplitude, initialPeriod, initialPhase))];
+  readFile('/assets/residuals/residuals.26.dat')
+  .then(result => {
+    // Process the result here
+    const { days, residuals } = result;
+    console.log("days:", days);
+    console.log("residuals:", residuals);
 
-  let amplitude = initialAmplitude;
-  let period = initialPeriod;
-  let phase = initialPhase;
-  let residual = residual_calc(initialSin[0], initialSin[0]);
+    const initialAmplitude = 1;
+    const initialPeriod = 0.1;
+    const initialPhase = 0;
+    const initialSin = [days.map(x => sinEquation(x, initialAmplitude, initialPeriod, initialPhase))];
 
-
-  const modelTrace = {
-    x: xRange,
-    y: initialSin,
-    type: 'scatter',
-    name: 'Orbit Model',
-  };
-  var inputDataTrace = {
-    x: xRange,
-    y: initialSin,
-    mode: 'markers',
-    type: 'scatter',
-    name: 'ToAs',
-    marker: { size: 12 }
-  };
-  var residualTrace = {
-    x: xRange,
-    y: residual,
-    mode: 'markers',
-    type: 'scatter',
-    name: 'Residual',
-    marker: { size: 12 }
-  };
+    let amplitude = initialAmplitude;
+    let period = initialPeriod;
+    let phase = initialPhase;
+    let fitResidual = residual_calc(initialSin[0], residuals);
+    console.log("initialSin:", initialSin);
 
 
-  const amplitudeSliderSteps = [];
-  const periodSliderSteps = [];
-  const phaseSliderSteps = [];
-  for (let a = 0; a <= 10; a += 0.1) {
-    amplitudeSliderSteps.push({
-      method: 'relayout',
-      label: a.toFixed(1),
-      args: ['yaxis.range', [-10, 10]],
-    });
-    periodSliderSteps.push({
-      method: 'relayout',
-      label: 2 * a.toFixed(1),
-      args: ['yaxis.range', [-10, 10]],
-    });
-    phaseSliderSteps.push({
-      method: 'relayout',
-      label: a.toFixed(1),
-      args: ['yaxis.range', [-10, 10]],
-    });
-  }
+    const modelTrace = {
+      x: days,
+      y: initialSin,
+      type: 'scatter',
+      name: 'Orbit Model',
+    };
+    var inputDataTrace = {
+      x: days,
+      y: [residuals],
+      mode: 'markers',
+      type: 'scatter',
+      name: 'ToAs',
+      marker: { size: 12 }
+    };
+    var residualTrace = {
+      x: days,
+      y: fitResidual,
+      mode: 'markers',
+      type: 'scatter',
+      name: 'Residual',
+      marker: { size: 12 }
+    };
 
-  const sliders = [
-    {
-      steps: amplitudeSliderSteps,
-      active: 10,  // Initial position of the slider
-      currentvalue: {
-        prefix: 'Amplitude: ',
-        xanchor: 'left',
-        visible: true,
-        offset: 25,
-      },
-    },
-    {
-      steps: periodSliderSteps,
-      active: 10,  // Initial position of the slider
-      currentvalue: {
-        prefix: 'Period: ',
-        xanchor: 'center',
-        visible: true,
-        offset: 75,
-      },
-    },
-    {
-      steps: phaseSliderSteps,
-      active: 10,  // Initial position of the slider
-      currentvalue: {
-        prefix: 'Phase: ',
-        xanchor: 'right',
-        visible: true,
-        offset: 125,
-      },
-    },
-  ];
 
-  const layout = {
-    width: 800,
-    height: 600,
-    xaxis: { title: 'MJD' },
-    yaxis: { title: 'ToAs' },
-    sliders: sliders,
-  };
-
-  Plotly.newPlot('plot', [modelTrace, inputDataTrace], layout);
-  Plotly.restyle('plot', {'y': initialSin}, 0);
-  Plotly.restyle('plot', {'y': initialSin}, 1);
-  const plot = document.getElementById('plot');
-
-  const residualLayout = {
-    width: 800,
-    height: 400,
-    xaxis: { title: 'MJD' },
-    yaxis: { title: 'ToA Residual', autorange: false, range: [-10, 10] },
-  };
-  Plotly.newPlot('residual', [residualTrace], residualLayout);
-  Plotly.restyle('residual', {'y': [residual]}, 0);
-
-  plot.on('plotly_sliderchange', (event) => {
-    if (event.slider.currentvalue.prefix.startsWith('Amplitude')) {
-      amplitude = parseFloat(event.step.label);
+    const amplitudeSliderSteps = [];
+    const periodSliderSteps = [];
+    const phaseSliderSteps = [];
+    for (let a = 0; a <= 10; a += 0.1) {
+      amplitudeSliderSteps.push({
+        method: 'relayout',
+        label: a.toFixed(1),
+        args: ['yaxis.range', [-10, 10]],
+      });
     }
-    if (event.slider.currentvalue.prefix.startsWith('Period')) {
-      period = parseFloat(event.step.label);
+    for (let a = 0; a <= 0.1; a += 0.001) {
+      periodSliderSteps.push({
+        method: 'relayout',
+        label: a.toFixed(3),
+        args: ['yaxis.range', [-10, 10]],
+      });
     }
-    if (event.slider.currentvalue.prefix.startsWith('Phase')) {
-      phase = parseFloat(event.step.label);
+    for (let a = 0; a <= 1; a += 0.01) {
+      phaseSliderSteps.push({
+        method: 'relayout',
+        label: a.toFixed(2),
+        args: ['yaxis.range', [-10, 10]],
+      });
     }
-    const newY = xRange.map(x => sinEquation(x, amplitude, period, phase));
-    Plotly.restyle('plot', {'y': [newY]}, 0);
 
-    // Update residual
-    const newResidual = residual_calc(initialSin[0], newY);
-    Plotly.restyle('residual', {'y': [newResidual]}, 0);
+    const sliders = [
+      {
+        steps: amplitudeSliderSteps,
+        active: 10,  // Initial position of the slider
+        currentvalue: {
+          prefix: 'Amplitude: ',
+          xanchor: 'left',
+          visible: true,
+          offset: 25,
+        },
+      },
+      {
+        steps: periodSliderSteps,
+        active: 10,  // Initial position of the slider
+        currentvalue: {
+          prefix: 'Period: ',
+          xanchor: 'center',
+          visible: true,
+          offset: 75,
+        },
+      },
+      {
+        steps: phaseSliderSteps,
+        active: 10,  // Initial position of the slider
+        currentvalue: {
+          prefix: 'Phase: ',
+          xanchor: 'right',
+          visible: true,
+          offset: 125,
+        },
+      },
+    ];
+
+    const layout = {
+      width: 800,
+      height: 600,
+      xaxis: { title: 'MJD' },
+      yaxis: { title: 'ToA residuals (ms)' },
+      sliders: sliders,
+    };
+
+    Plotly.newPlot('plot', [modelTrace, inputDataTrace], layout);
+    Plotly.restyle('plot', {'y': initialSin}, 0);
+    Plotly.restyle('plot', {'y': [residuals]}, 1);
+    const plot = document.getElementById('plot');
+
+    const residualLayout = {
+      width: 800,
+      height: 400,
+      xaxis: { title: 'MJD' },
+      yaxis: { title: 'Residual between model and fit', autorange: false, range: [-10, 10] },
+    };
+    Plotly.newPlot('residual', [residualTrace], residualLayout);
+    Plotly.restyle('residual', {'y': [fitResidual]}, 0);
+
+    plot.on('plotly_sliderchange', (event) => {
+      if (event.slider.currentvalue.prefix.startsWith('Amplitude')) {
+        amplitude = parseFloat(event.step.label);
+      }
+      if (event.slider.currentvalue.prefix.startsWith('Period')) {
+        period = parseFloat(event.step.label);
+      }
+      if (event.slider.currentvalue.prefix.startsWith('Phase')) {
+        phase = parseFloat(event.step.label);
+      }
+      const newY = days.map(x => sinEquation(x, amplitude, period, phase));
+      Plotly.restyle('plot', {'y': [newY]}, 0);
+
+      // Update residual
+      const newResidual = residual_calc(initialSin[0], newY);
+      Plotly.restyle('residual', {'y': [newResidual]}, 0);
+    });
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('Error:', error);
   });
 });
